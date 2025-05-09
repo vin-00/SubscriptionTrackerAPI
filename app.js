c0c5a7e709135b850a447939faa0f8f111601fce
vinay <vinaykhatri292@gmail.com>
2025-05-09 22:28:30 +0530

    added models

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import subscriptionRouter from './routes/subscriptions.routes.js';
 import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js';
 
+import connectToDatabase from './database/mongodb.js';
+
 const app = express();
 
 app.use("/api/v1/auth",authRouter);
@@ -16,8 +18,10 @@ app.get("/" , (req,res)=>{
     res.send("Welcome to the subscription tracker api");
 })
 
-app.listen(PORT,()=>{
+app.listen(PORT,async ()=>{
     console.log(`Subscription tracker API is running on http://localhost:${PORT}`);
+
+    await connectToDatabase();
 })
 
 export default app;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,17 +3,17 @@ import {Router} from 'express';
 const authRouter = Router();
 
 authRouter.post("/sign-up", (req, res) => {
-  // Handle user signup logic here
+
   res.send({title: "User signed up"});
 });
 
 authRouter.post("/sign-in", (req, res) => {
-  // Handle user signup logic here
+
   res.send({title:"User signed up"});
 });
 
 authRouter.post("/sign-out", (req, res) => {
-  // Handle user signup logic here
+
   res.send({title:"User signed up"});
 });
 
